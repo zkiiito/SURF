dd187f9be1b2ee333856a0787f62cb82a0d89b29
zooli <zkiiito@gmail.com>
2013-01-20 12:30:18 +0100

    #12 offline userek nem mutatva

--- client/js/view/wave.view.js
+++ client/js/view/wave.view.js
@@ -2,7 +2,7 @@ var WaveView = Backbone.View.extend({
     initialize: function() {
         _.bindAll(this, 'setCurrent', 'addMessage', 'addUser', 'removeUser', 'updateTitle', 
                         'showUpdateWave', 'scrollToNextUnread', 'scrollToBottom', 'readAllMessages',
-                        'quitWave', 'removeWave');
+                        'quitWave', 'removeWave', 'countOfflineUsers');
         
         this.userViews = [];
         
@@ -14,6 +14,7 @@ var WaveView = Backbone.View.extend({
         this.model.messages.bind('add', this.addMessage);
         this.model.users.bind('add', this.addUser);
         this.model.users.bind('remove', this.removeUser);
+        this.model.users.bind('change', this.countOfflineUsers)
     },
     events: {
         'click a.editwave' : 'showUpdateWave',
@@ -99,20 +100,34 @@ var WaveView = Backbone.View.extend({
         var usernames = this.model.getUserNames();
         this.$el.find('span.usernames').text(usernames);
     },
-    
+        
     addUser: function(user) {
         this.changeUsers();
         var userView = new UserView({model: user});
         this.userViews[user.id] = userView;
         
         this.$el.find('.heads').append(userView.render().el);
+        this.countOfflineUsers();
     },
     
     removeUser: function(user) {
         this.changeUsers();
         this.userViews[user.id].remove();
         delete this.userViews[user.id];
+        this.countOfflineUsers();
     },
+    
+    countOfflineUsers: function() {
+        var counter = this.$el.find('.offline-list'),
+            offlineCount = this.model.users.where({status: 'offline'}).length;
+        
+        if (offlineCount > 0) {
+            counter.find('.count').text(offlineCount);
+            counter.show();
+        } else {
+            counter.hide();
+        }
+    },    
     
     updateTitle: function() {
         this.$el.find('h2').text(this.model.get('title'));
