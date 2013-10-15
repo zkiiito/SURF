444e910b3a0030758195feb49123f1b3c22c96d2
zooli <zkiiito@gmail.com>
2013-10-15 23:59:48 +0200

    case sensitive

--- code/Model.js
+++ code/Model.js
@@ -3,7 +3,7 @@ module.exports = {
     Message: require('./model/Message'),
     MessageCollection: require('./model/MessageCollection'),
     User: require('./model/User'),
-    UserCollection: require('./model/userCollection'),
+    UserCollection: require('./model/UserCollection'),
     Wave: require('./model/Wave'),
     WaveCollection: require('./model/WaveCollection')
 };
