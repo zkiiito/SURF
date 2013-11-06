5664d950f8cca03421b9d3c4609557358b1a9180
zooli <zkiiito@gmail.com>
2013-11-06 22:52:16 +0100

    configok windows alatt

---
+++ surf.bat
@@ -0,0 +1,8 @@
+@echo off
+set GOOGLE_APPID=290177368237.apps.googleusercontent.com
+set GOOGLE_APPSECRET=x58fnA7rUYCqhsLeAXTakjdN
+set FACEBOOK_APPID=540926909316211
+set FACEBOOK_APPSECRET=ca1521da4e2d0dac4dc3978580d25ae6
+set MONGOLAB_URI=mongodb://localhost/wave0
+set REDISCLOUD_URL=redis://localhost:6379
+node WaveChat.js
