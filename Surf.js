f45011c21eb9e0a0a9fd2e6e558281b0eded59f7
Feher Zoltan <feher.zoltan@arkon.hu>
2015-06-30 22:04:02 +0200

    nodetime shut down on heroku

--- Surf.js
+++ Surf.js
@@ -1,11 +1,4 @@
 var SurfServer = require('./code/SurfServer'),
     Config = require('./code/Config');
 
-if (Config.nodetimeKey) {
-    require('nodetime').profile({
-        accountKey: Config.nodetimeKey,
-        appName: 'surf'
-    });
-}
-
 SurfServer.init();
