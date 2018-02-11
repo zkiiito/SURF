4fc0022963b5778e497ead5851599f4761f52853
zooli <zkiiito@gmail.com>
2018-02-11 21:52:39 +0100

    replaced: grunt-contrib-uglify-es added: nodemon

--- gruntfile.js
+++ gruntfile.js
@@ -2,60 +2,60 @@ module.exports = function (grunt) {
     'use strict';
 
     grunt.loadNpmTasks('grunt-eslint');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-uglify-es');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-text-replace');
 
     var jsFiles = [
-        "node_modules/socket.io-client/dist/socket.io.js",
-        "node_modules/underscore/underscore.js",
-        "node_modules/backbone/backbone.js",
-        "node_modules/R.js/R.js",
-        "client/js/i18n.js",
-        "client/js/jquery/jquery.tokeninput.js",
-        "node_modules/dateformat/lib/dateformat.js",
-        "node_modules/phpjs/functions/strings/strip_tags.js",
-        "node_modules/phpjs/functions/strings/nl2br.js",
-        "node_modules/phpjs/functions/strings/wordwrap.js",
-        "node_modules/sweetalert/dist/sweetalert.min.js",
-        "client/js/randomname.js",
-        "client/js/model/user.model.js",
-        "client/js/model/wave.model.js",
-        "client/js/model/message.model.js",
-        "client/js/model/surfapp.model.js",
-        "client/js/view/user.view.js",
-        "client/js/view/wavelist.view.js",
-        "client/js/view/wave.view.js",
-        "client/js/view/message.view.js",
-        "client/js/view/wavereplyform.view.js",
-        "client/js/view/messagereplyform.view.js",
-        "client/js/view/editwave.view.js",
-        "client/js/view/edituser.view.js",
-        "client/js/view/disconnected.view.js",
-        "client/js/view/surfapp.view.js",
-        "client/js/communicator.js",
-        "client/js/surf.js"
+        'node_modules/socket.io-client/dist/socket.io.js',
+        'node_modules/underscore/underscore.js',
+        'node_modules/backbone/backbone.js',
+        'node_modules/R.js/R.js',
+        'client/js/i18n.js',
+        'client/js/jquery/jquery.tokeninput.js',
+        'node_modules/dateformat/lib/dateformat.js',
+        'node_modules/phpjs/functions/strings/strip_tags.js',
+        'node_modules/phpjs/functions/strings/nl2br.js',
+        'node_modules/phpjs/functions/strings/wordwrap.js',
+        'node_modules/sweetalert/dist/sweetalert.min.js',
+        'client/js/randomname.js',
+        'client/js/model/user.model.js',
+        'client/js/model/wave.model.js',
+        'client/js/model/message.model.js',
+        'client/js/model/surfapp.model.js',
+        'client/js/view/user.view.js',
+        'client/js/view/wavelist.view.js',
+        'client/js/view/wave.view.js',
+        'client/js/view/message.view.js',
+        'client/js/view/wavereplyform.view.js',
+        'client/js/view/messagereplyform.view.js',
+        'client/js/view/editwave.view.js',
+        'client/js/view/edituser.view.js',
+        'client/js/view/disconnected.view.js',
+        'client/js/view/surfapp.view.js',
+        'client/js/communicator.js',
+        'client/js/surf.js'
     ];
 
     var jsFilesAdmin = [
-        "node_modules/underscore/underscore.js",
-        "node_modules/backbone/backbone.js",
-        "admin/js/lib/backbone.paginator.js",
-        "admin/js/lib/bootstrap.js",
-        "admin/js/lib/backgrid.js",
-        "admin/js/lib/extensions/paginator/backgrid-paginator.js",
-        "admin/js/lib/extensions/text-cell/backgrid-text-cell.js",
-        "admin/js/admin.js",
-        "admin/js/model/message.model.js",
-        "admin/js/model/user.model.js",
-        "admin/js/model/wave.model.js",
-        "admin/js/model/waveinvite.model.js",
-        "admin/js/view/user.view.js"
+        'node_modules/underscore/underscore.js',
+        'node_modules/backbone/backbone.js',
+        'admin/js/lib/backbone.paginator.js',
+        'admin/js/lib/bootstrap.js',
+        'admin/js/lib/backgrid.js',
+        'admin/js/lib/extensions/paginator/backgrid-paginator.js',
+        'admin/js/lib/extensions/text-cell/backgrid-text-cell.js',
+        'admin/js/admin.js',
+        'admin/js/model/message.model.js',
+        'admin/js/model/user.model.js',
+        'admin/js/model/wave.model.js',
+        'admin/js/model/waveinvite.model.js',
+        'admin/js/view/user.view.js'
     ];
 
     var cssFiles = [
-        "client/css/style.css",
-        "client/css/token-input.css"
+        'client/css/style.css',
+        'client/css/token-input.css'
     ];
 
     grunt.initConfig({
