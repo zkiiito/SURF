c09751e0cf04780f08878dc910774c548ec414fb
zoltan feher <zoli@kasa.com>
2023-04-08 20:55:57 +0200

    build size

--- client-react/vite.config.ts
+++ client-react/vite.config.ts
@@ -1,9 +1,19 @@
-import { defineConfig } from 'vite'
+import { PluginOption, defineConfig } from 'vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      template: 'treemap', // or sunburst
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+      filename: 'analice.html',
+    }) as PluginOption,
+  ],
   server: {
     port: 3000,
   },
