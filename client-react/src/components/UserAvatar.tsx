55d6465ec4eabeec0695c671d13a6d49ebcf0657
zoltan feher <zoli@kasa.com>
2026-02-02 13:04:51 +0100

    fix avatar

--- client-react/src/components/UserAvatar.tsx
+++ client-react/src/components/UserAvatar.tsx
@@ -4,11 +4,20 @@ interface Props {
   user: User
 }
 
+function getAvatarSrc(avatar: string): string {
+  // If avatar is already a full URL (e.g., Google OAuth profile picture), use it directly
+  if (avatar.startsWith('http://') || avatar.startsWith('https://')) {
+    return avatar
+  }
+  // Otherwise, treat it as a local image name
+  return `/images/${avatar}.png`
+}
+
 export default function UserAvatar({ user }: Props) {
   return (
     <img 
       className={user.status} 
-      src={`/images/${user.avatar}.png`} 
+      src={getAvatarSrc(user.avatar)} 
       alt={user.name} 
       title={user.name}
     />
